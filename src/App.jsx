fd0b98b9581ef7f5e31e9a7b408310419039d361
jaisleek <babajide.babalola@yahoo.com>
2025-06-22 21:13:47 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+import About from './components/About'
+import CompanyLogo from './components/CompanyLogo'
+import CustomerSection from './components/CustomerSection'
+import DesignSection from './components/DesignSection'
+import FeaturesSection from './components/FeaturesSection'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import TryNow from './components/TryNow'
+
+function App() {
+
+  
+  return (  
+    <>
+   <Navbar />
+   <Hero/>
+   <CompanyLogo/>
+   <FeaturesSection/>
+   <DesignSection/>
+   <CustomerSection/>
+   <About/>
+   <TryNow/>
+    </>
+  )
+}
+
+export default App
